9de3cb2fbb0e5f8d1efd591ae42e43457042ef7b
SAKIB RAHMAN <srskb15@gmail.com>
2016-10-14 00:31:44 +0600

    List Data Type

--- list.py
+++ list.py
@@ -15,3 +15,26 @@ print(list_two[0][0])
 print(list_two[0][1])
 print(list_two[1])
 print(list_two[2])
+
+# negative indexes
+print(li[-1])
+print(li[-2])
+print(li[-3])
+
+# changing value of list
+li[0] = "tiger"
+li[0] = "lion"
+print(li)
+
+# list Concatenation
+li = li + list_two
+print(li)
+
+spam = [1, 2, 3]
+spam = spam + ['A', 'B', 'C']
+print(spam)
+
+# delete list item
+del spam[2]
+print(spam)
+
