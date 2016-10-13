55d8c3757ab16cf537a2513562a4dcd29f176bd9
SAKIB RAHMAN <srskb15@gmail.com>
2016-10-14 00:23:02 +0600

    Loop Operation

---
+++ list.py
@@ -0,0 +1,19 @@
+li = ["cat ","rat", "mouse",1.26,2]
+
+print(li) # print all the list values
+
+#getting individual value in a list with indexes
+print(li[0])
+print(li[1])
+print(li[2])
+print(li[-1]) # -1 index represent the last value of the list
+
+#list can contain other list values
+list_two = [["cat","bat"],1,2,3]
+print(list_two[0])
+print(list_two[0][0])
+print(list_two[0][1])
+print(list_two[1])
+print(list_two[2])
+
+
