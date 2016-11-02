465bd9986671d7013a6e5bb677208a139d7d84b7
SAKIB RAHMAN <srskb15@gmail.com>
2016-11-02 08:19:25 +0600

    Dictonaries and Structuring Data

--- Dictionary.py
+++ Dictionary.py
@@ -1,7 +1,22 @@
-book = {'name': 'Automate the boring stuff', 'type': 'Python'}
+book = {'name': 'Python Learing', 'type': 'Python'}
 print(book['name'])
+print(book['type'])
 
+# print all the keys
 for v in book.keys():
 	print(v)
+# print all the values of keys
 for k in book.values():
 	print(k)
+
+... == == == == == == == == == == == ==
+"List Vs Distionary"
+== == == == == == == == == == == == ...
+spam = ['cats', 'dogs', 'mouse']
+bacon = ['dogs', 'mouse', 'cats']
+print(spam == bacon)  # it return false . Order of item matter whether two list are same or not
+
+personOne = {'name': 'john doe', 'age': 8, 'birthday': '10-05-1995'}
+personTwo = {'birthday': '10-05-1995', 'name': 'john doe', 'age': 8}
+
+print(personOne == personTwo)  # it return true . Order of items doesn't matter whether two dictionaries are same or not
